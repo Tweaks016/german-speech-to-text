d363f285c32d1b5baf5e32d21a4881009181d385
Tweaks016 <69620902+Tweaks016@users.noreply.github.com>
2023-03-09 18:55:12 +0530

    Add files via upload

---
+++ mainApp.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for, redirect, render_template_string
+from app import RussianText
+from pydub import AudioSegment
+
+app = Flask(__name__)
+app.secret_key = 'John The Ripper'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def currentTemp():
+    if request.method == 'POST':
+        
+        wavFile = request.files['file']
+        audio_file = wavFile
+        filename = wavFile.filename        
+        if audio_file.content_type != 'audio/wav' and audio_file.content_type not in ('audio/mpeg', 'audio/mp3'):            
+            return redirect(url_for('home'))
+        if audio_file.content_type not in ('audio/mpeg', 'audio/mp3'):         
+            convertedText = RussianText.russianTextFromSpeech(audio_file)            
+        else:
+            audio = AudioSegment.from_file(audio_file)
+            wavFile = audio.export('output.wav', format='wav')
+            convertedText = RussianText.russianTextFromSpeech(wavFile)
+        if convertedText[0] != False:            
+            return redirect(url_for('home'))
+        return render_template('index.html', convertedText=convertedText[1])
+    return "Error"
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
