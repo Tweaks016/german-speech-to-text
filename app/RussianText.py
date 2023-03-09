b843f68fddc80f19e3e5f2abd5ac5f81be396a86
Tweaks016 <69620902+Tweaks016@users.noreply.github.com>
2023-03-09 18:53:41 +0530

    Add files via upload

---
+++ app/RussianText.py
@@ -0,0 +1,26 @@
+'''
+Text Conversion from russian speech to text.....
+'''
+def russianTextFromSpeech(wav_file):
+    text_generated = ''
+    try:
+        import speech_recognition as sr
+        from pydub import AudioSegment
+    except Exception as e:
+        print("[-] ModuleNotFoundError: ", e)
+
+    try:
+        r = sr.Recognizer()
+        with sr.AudioFile(wav_file) as source:
+            audio_text = r.listen(source)
+            try:
+                text = r.recognize_google(audio_text, language="ru-RU")
+                # print('[+] -> ', text)
+                text_generated = text
+                return False, text_generated
+            except Exception as err:
+                # print(f"[-] Error Found: {err}")
+                pass
+    except Exception as er:
+        # print(f"[-] Error Occured: {er}")
+        pass
